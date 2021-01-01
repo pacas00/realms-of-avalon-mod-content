dff05167df1c8445d984da6edeb83d35b38cb9c5
Peter Cashel <pacas00@gmail.com>
2021-01-01 21:20:53 +1000

    feat: Custom Server List screen for modpack. Doesn't effect Multiplayer Screen.

--- src/main/java/net/petercashel/RealmsOfAvalonMod/RealmsOfAvalonEventHandler.java
+++ src/main/java/net/petercashel/RealmsOfAvalonMod/RealmsOfAvalonEventHandler.java
@@ -1,13 +1,22 @@
 package net.petercashel.RealmsOfAvalonMod;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiMainMenu;
+import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityMinecartChest;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.text.TextComponentString;
+import net.minecraftforge.client.event.GuiOpenEvent;
+import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.fml.common.eventhandler.Event;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+import net.petercashel.RealmsOfAvalonMod.GUI.Servers.GuiMultiplayerPack;
 
 public class RealmsOfAvalonEventHandler {
 
@@ -40,5 +49,39 @@ public class RealmsOfAvalonEventHandler {
                 }
             }
         }
+    }
+
+
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    @SideOnly(Side.CLIENT)
+    public static void openGui(GuiOpenEvent event) {
+        if (event.getGui() instanceof GuiMainMenu) {
+            GuiMainMenu mm = (GuiMainMenu) event.getGui();
+
+        }
+
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    @SideOnly(Side.CLIENT)
+    public static void onScreenEvent(GuiScreenEvent.InitGuiEvent event) {
+        if (event.getGui() instanceof GuiMainMenu) {
+            GuiMainMenu mm = (GuiMainMenu) event.getGui();
+
+        }
+
+    }
+
+    @SubscribeEvent(priority = EventPriority.LOW)
+    @SideOnly(Side.CLIENT)
+    public static void onAPEvent(GuiScreenEvent.ActionPerformedEvent.Post event) {
+        GuiButton b = event.getButton();
+        if (b.displayString.equals("Official Servers") && b.id >= 6000) //Hopefully, Custom main menu button.
+        {
+            GuiMultiplayerPack pack = new GuiMultiplayerPack(event.getGui());
+            Minecraft.getMinecraft().displayGuiScreen((GuiScreen)pack);
+        }
+
     }
 }
